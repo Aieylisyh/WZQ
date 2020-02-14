173225176726661361bf673102f341c55e409761
Aieylisyh <836823389@qq.com>
2020-02-14 22:47:56 +0800

    wip

---
+++ assets/scripts/five/simpleAnimations/Pudding.js
@@ -0,0 +1,46 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        minScale: 0.5,
+
+        maxScale: 1,
+
+        turnTime: 1,
+
+        loop: true,
+
+        playOnEnable: true,
+
+        isPlaying: false,
+
+        timer:0,
+    },
+
+
+    onLoad() {
+        this.deltaScale = this.maxScale - this.minScale;
+    },
+
+    onEnable() {
+        if (this.playOnEnable) {
+            this.isPlaying = true;
+        }
+    },
+
+    update(dt) {
+        if (!this.isPlaying) {
+            return;
+        }
+
+        let f = this.timer / this.turnTime;//f=0 big f=1 big f=0.5 small
+        let factor1 = Math.cos(f * Math.PI * 2);
+        let factor2 = Math.sin(f * Math.PI * 2);
+        this.node.scaleX = this.minScale + this.deltaScale * factor1;
+        this.node.scaleY = this.minScale + this.deltaScale * factor2;
+        this.timer -= dt;
+        if (this.timer < 0) {
+            this.timer += this.turnTime;
+        }
+    },
+});
